e5052e7953fa08fce3e9d5c4ebd8ef0f5418b574
Magdiel Cordero <contact@magdielcordero.info>
2026-05-29 14:58:14 -0600

    testing

--- src/supabase.js
+++
@@ -1,5 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://hagseiwqouapirtlbume.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhhZ3NlaXdxb3VhcGlydGxidW1lIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAwNjIzMzYsImV4cCI6MjA5NTYzODMzNn0.9dy-1MyzPtLU86MLLwRGqYO5BwzDywsLgYg0QKRZ2o0';
-export const supabase = createClient(supabaseUrl, supabaseKey);
